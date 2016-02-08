292cfbe731ed0deee6c935d69f626dc356ad8eb5
Shaun Sundquist <sund@unc.edu>
2016-02-08 12:56:52 -0500

    for version 3

--- homebrew-b2sync.rb
+++ homebrew-b2sync.rb
@@ -3,7 +3,7 @@ class HomebrewB2sync < Formula
   homepage "https://github.com/sund/homebrew-b2sync"
   url "https://github.com/sund/homebrew-b2sync/archive/v0.3.tar.gz"
   version "0.3"
-  sha256 ""
+  sha256 "bf721362204311c692838af9fbc1fe537a04e7607d6e78b8f4ed47b959dc7119"
 
   def install
       bin.install "b2syncinstall.sh"
