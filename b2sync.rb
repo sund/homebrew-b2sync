0ef2d2366b721578d40bbea8138b67a770fa5677
Shaun Sundquist <sund@unc.edu>
2016-02-09 12:33:53 -0500

    fix some audit issues

--- b2sync.rb
+++ b2sync.rb
@@ -1,17 +1,17 @@
 class B2sync < Formula
-  desc "b2sync installer for brew"
+  desc "backblaze b2sync installer for brew"
   homepage "https://github.com/sund/homebrew-b2sync"
   url "https://github.com/sund/homebrew-b2sync/archive/v0.5.tar.gz"
   version "0.5"
   sha256 "e3fc7fd87c46d186f7fefc8451d87deb624496b38193914e3bb7fce08c7089ad"
 
-  def install
-      bin.install "b2sync"
-      system "sudo mkdir -p /Library/Logs/b2sync/logs"
-      system "sudo touch /Library/Logs/b2sync/b2sync"
-  end
+def install
+  bin.install "b2sync"
+  system "sudo", "mkdir", "-p", "/Library/Logs/b2sync/logs"
+  system "sudo", "touch", "/Library/Logs/b2sync/b2sync"
+end
 
-  test do
-      system "#{bin}/b2sync", "-otheroption", "-version"
+test do
+    system "#{bin}/b2sync", "-otheroption", "-version"
   end
 end
