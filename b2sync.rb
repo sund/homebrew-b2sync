84513033a589194246d3498b866faf40e36a2524
Shaun Sundquist <sund@mac.com>
2016-02-09 11:10:55 -0500

    new version

--- b2sync.rb
+++ b2sync.rb
@@ -1,9 +1,9 @@
-class HomebrewB2sync < Formula
+class 2sync < Formula
   desc "b2sync installer for brew"
   homepage "https://github.com/sund/homebrew-b2sync"
-  url "https://github.com/sund/homebrew-b2sync/archive/v0.4.tar.gz"
-  version "0.4"
-  sha256 "535c7d06453938b07732aec23835c8b8b8749dfc3f3f77848e869466d54e81f7"
+  url "https://github.com/sund/homebrew-b2sync/archive/v0.5.tar.gz"
+  version "0.5"
+  sha256 ""
 
   def install
       bin.install "b2sync"
