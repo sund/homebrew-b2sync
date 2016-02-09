6fd166859c4f0540f0e3e65626f28dcb560d240c
Shaun Sundquist <sund@unc.edu>
2016-02-09 10:33:57 -0500

    version 4

--- homebrew-b2sync.rb
+++ homebrew-b2sync.rb
@@ -1,8 +1,8 @@
 class HomebrewB2sync < Formula
   desc "b2sync installer for brew"
   homepage "https://github.com/sund/homebrew-b2sync"
-  url "https://github.com/sund/homebrew-b2sync/archive/v0.3.tar.gz"
-  version "0.3"
+  url "https://github.com/sund/homebrew-b2sync/archive/v0.4.tar.gz"
+  version "0.4"
   sha256 "bf721362204311c692838af9fbc1fe537a04e7607d6e78b8f4ed47b959dc7119"
 
   def install
