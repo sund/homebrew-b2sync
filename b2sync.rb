ff492bcf14051c0d31e5723a307547a71e8db854
Shaun Sundquist <sund@mac.com>
2016-02-09 11:25:18 -0500

    really correct class

--- b2sync.rb
+++ b2sync.rb
@@ -1,4 +1,4 @@
-class b2sync < Formula
+class HomebrewB2sync < Formula
   desc "b2sync installer for brew"
   homepage "https://github.com/sund/homebrew-b2sync"
   url "https://github.com/sund/homebrew-b2sync/archive/v0.5.tar.gz"
