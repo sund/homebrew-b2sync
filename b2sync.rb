b5dc160e487d59172111bdb6c168c6975e4ca301
Shaun <sund@mac.com>
2016-02-10 20:55:06 -0500

    update homepage
    Issue #4

--- b2sync.rb
+++ b2sync.rb
@@ -1,8 +1,8 @@
 class B2sync < Formula
   desc "Backblaze's command line rsync-like utility for B2"
-  homepage "https://github.com/sund/homebrew-b2sync"
-  url "https://github.com/sund/homebrew-b2sync/archive/v0.5.tar.gz"
-  version "0.5"
+  homepage "http://sund.github.io/homebrew-b2sync"
+  url "https://github.com/sund/homebrew-b2sync/archive/0.6.0.4-1.0.tar.gz"
+  version "0.6.0.4-1.0"
   sha256 "e3fc7fd87c46d186f7fefc8451d87deb624496b38193914e3bb7fce08c7089ad"
 
   def install
